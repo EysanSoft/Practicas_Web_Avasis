d676da1a7e53089572c3ef8841ebcc6955ed042a
EysanSoft <ivanhersan16@gmail.com>
2025-01-22 10:44:55 -0600

    Pequeña correción en la tabla PDF...

--- Practica_4/scripts/php/peticion_descargar_usuarios_en_pdf.php
+++ Practica_4/scripts/php/peticion_descargar_usuarios_en_pdf.php
@@ -25,10 +25,11 @@ else {
     // Inicializar Dompdf.
     $dompdf = new Dompdf($options);
 
-    // For magico.
+    // For mágico.
     $magia = "";
     for ($i = 0; $i < count($dataArray); $i++) {
         $magia .= '<tr>';
+        $magia .= '<td>' . $dataArray[$i] -> userId . '</td>';
         $magia .= '<td>' . $dataArray[$i] -> name . '</td>';
         $magia .= '<td>' . $dataArray[$i] -> lastName . '</td>';
         $magia .= '<td>' . $dataArray[$i] -> phone . '</td>';
@@ -39,68 +40,67 @@ else {
     // Escribir y cargar HTML.
     $docHTML = "
         <!DOCTYPE html>
-    <html lang='en'>
-        <head>
-            <meta charset='UTF-8' />
-            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-            <title>Usuarios Avasis API</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                }
-                h1 {
-                    text-align: center;
-                }
-                table {
-                    width: 100%;
-                    border-collapse: collapse;
-                }
-                th {
-                    background-color:rgb(255, 205, 205);
-                    height: 3em;
-                }
-                td {
-                    text-align: center;
-                }
-                tr:nth-child(even) {
-                    background-color:rgb(255, 200, 200);
-                }
-                tr:nth-child(odd) {
-                    background-color:rgb(255, 175, 175);
-                }
-                table,
-                th,
-                td {
-                    border: 1px solid black;
-                }
-            </style>
-        </head>
-        <body>
-            <div>
-                <h1>Usuarios Avasis API</h1>
-                <table>
-                    <thead>
-                        <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        "
-                        .
-                        $magia
-                        .
-                        "
-                    </tbody>
-                </table>
-            </div>
-        </body>
-    </html>
+        <html lang='en'>
+            <head>
+                <meta charset='UTF-8' />
+                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+                <title>Usuarios Avasis API</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                    }
+                    h1 {
+                        text-align: center;
+                    }
+                    table {
+                        width: 100%;
+                        border-collapse: collapse;
+                    }
+                    th {
+                        background-color:rgb(255, 205, 205);
+                        height: 3em;
+                    }
+                    td {
+                        text-align: center;
+                    }
+                    tr:nth-child(even) {
+                        background-color:rgb(255, 200, 200);
+                    }
+                    tr:nth-child(odd) {
+                        background-color:rgb(255, 175, 175);
+                    }
+                    table,
+                    th,
+                    td {
+                        border: 1px solid black;
+                    }
+                </style>
+            </head>
+            <body>
+                <div>
+                    <h1>Usuarios Avasis API</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Telefono</th>
+                            <th>Correo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            "
+                            .
+                            $magia
+                            .
+                            "
+                        </tbody>
+                    </table>
+                </div>
+            </body>
+        </html>
     ";
-
-    // $docHTML = "../../views/tabla_usuarios.php";
 
     $dompdf->loadHtml($docHTML);
 
@@ -110,10 +110,8 @@ else {
     // Renderizar PDF.
     $dompdf->render();
 
-    /*
     // Salida del PDF en el navegador.
-    $dompdf->stream("usuarios_avasis_api.pdf");
-    */
+    // $dompdf->stream("usuarios_avasis_api.pdf");
 
     // Salida del PDF guardado en la carpeta resources.
     $output = $dompdf->output();
@@ -122,3 +120,4 @@ else {
     $response = ["status" => true, "message" => "Archivo PDF descargado."];
     echo json_encode($response);
 }
+?>
